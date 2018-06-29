4632b130d677e48ea4baf83828206c0a97c8f764
Diego Fernández Barrera <bigomby@gmail.com>
2018-06-29 11:39:51 +0200

    :bug: Fix import

--- src/service-client/index.ts
+++ src/service-client/index.ts
@@ -1,13 +1,12 @@
 import axios from 'axios';
 
 import { Wallet } from '../wallet';
-import { ClientOptions } from '../options';
 import { RequestSigner } from '../request-signer';
 
 export abstract class ServiceClient {
   constructor(
     private readonly requestSigner: RequestSigner,
-    private readonly options: ClientOptions,
+    private readonly options: any,
     protected readonly http = axios.create({ baseURL: options.url }),
   ) {
     http.interceptors.request.use(requestSigner.getInterceptor());
